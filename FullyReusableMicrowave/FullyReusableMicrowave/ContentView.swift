a893da328dc7740e6daeaf58cb4391c672f16981
Marco Puig <90495366+Marco-Puig@users.noreply.github.com>
2024-05-15 21:57:35 -0400

    Window

---
+++ FullyReusableMicrowave/FullyReusableMicrowave/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FullyReusableMicrowave
+//
+//  Created by Marco Puig on 5/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Window!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FullyReusableMicrowave/FullyReusableMicrowave/FullyReusableMicrowaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  FullyReusableMicrowaveApp.swift
+//  FullyReusableMicrowave
+//
+//  Created by Marco Puig on 5/15/24.
+//
+
+import SwiftUI
+
+@main
+struct FullyReusableMicrowaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
