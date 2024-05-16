3061d8338b80a503ddce18b410e628fbf1d3dc22
Robert Morelli <robertondino@outlook.com>
2024-05-15 20:58:53 -0600

    add skeleton to metal view

--- FullyReusableMicrowave/FullyReusableMicrowave/ContentView.swift
+++ FullyReusableMicrowave/FullyReusableMicrowave/ContentView.swift
@@ -6,16 +6,32 @@
 //
 
 import SwiftUI
+import MetalKit
+
+struct MTKMapView: NSViewRepresentable {
+    typealias NSViewType = MTKView;
+    var internalSelf: MTKView;
+    
+    init(){
+        self.internalSelf = MTKView();
+    }
+    
+    func makeNSView(context: Context) -> MTKView {
+        return internalSelf;
+    }
+    
+    func updateNSView(_ nsView: MTKView, context: Context) {
+    }
+}
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
             Text("Window!")
+                .padding()
+            MTKMapView() // Placeholder for the Metal view
+                .frame(height: 300) // Set a height for the Metal view
         }
-        .padding()
     }
 }
 
