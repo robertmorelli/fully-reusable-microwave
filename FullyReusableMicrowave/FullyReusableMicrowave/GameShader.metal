d56caf4a21820fa995565c87a96f98d842923780
Robert Morelli <robertondino@outlook.com>
2024-05-16 23:48:12 -0600

    kernel exists and screen size passed to frag

--- FullyReusableMicrowave/FullyReusableMicrowave/GameShader.metal
+++ FullyReusableMicrowave/FullyReusableMicrowave/GameShader.metal
@@ -5,21 +5,52 @@
 //  Created by Marco Puig on 5/16/24.
 //
 
+
+
 #include <metal_stdlib>
 using namespace metal;
 
-vertex float4 vertex_main(uint vertexID [[vertex_id]]) {
-    // Triangle Vertices
-    float4 vertices[3] = {
-        float4(-1.0, -1.0, 0.0, 1.0),
-        float4( 1.0, -1.0, 0.0, 1.0),
-        float4( 0.0,  1.0, 0.0, 1.0)
-    };
-    return vertices[vertexID];
+
+typedef enum {
+    air,
+    sand,
+    vapor,
+    fire,
+    water
+} cellType;
+
+
+typedef struct {
+    cellType type;
+} cell;
+
+
+kernel void update_world(){
+    
 }
 
-fragment half4 fragment_main() {
-    // Imaginary Techinque, Purple
-    return half4(0.8, 0.6, 0.8, 1.0);
+
+kernel void game_logic_step2(){
+    
 }
 
+
+kernel void game_logic_step3(){
+    
+}
+
+
+//done
+vertex float4 vertex_main(device float3 *vertices [[ buffer(0) ]], uint vertexID [[ vertex_id ]]) {
+    return float4(vertices[vertexID], 1);
+}
+
+
+fragment half4 fragment_main(device float *screenSize [[ buffer(0) ]],float4 fragCoord [[position]]) {
+    float x = fragCoord.x;
+    float y = fragCoord.y;
+    //TODO: get from game board
+    //TODO: shift by world position (camera position)
+    //TODO: get real ass screen size
+    return half4(x / screenSize[0], y / screenSize[1], 0.0, 1.0);
+}
--- FullyReusableMicrowave/FullyReusableMicrowave/GameView.swift
+++ FullyReusableMicrowave/FullyReusableMicrowave/GameView.swift
@@ -8,12 +8,37 @@
 import SwiftUI
 import MetalKit
 
+
+
+struct cell {
+    var id: Int
+}
+
+
 // Basic Renderer class implementation
 class Renderer {
     // Variables to hold Metal objects
     var device: MTLDevice?
     var commandQueue: MTLCommandQueue?
     var pipelineState: MTLRenderPipelineState?
+    var computePipelineState: MTLComputePipelineState?
+    
+    
+    static var fullscreenQuad: [SIMD3<Float>] = [
+        SIMD3<Float>( 1.0,  1.0, 0.0),
+        SIMD3<Float>( 1.0, -1.0, 0.0),
+        SIMD3<Float>( -1.0, 1.0, 0.0),
+        
+        SIMD3<Float>( -1.0, -1.0, 0.0),
+        SIMD3<Float>( 1.0, -1.0, 0.0),
+        SIMD3<Float>( -1.0, 1.0, 0.0)
+    ]
+    
+    var gameBoard: [cell] = Array(repeating: cell(id: 0), count: 512 * 512)
+    
+    var vertexBuffer: MTLBuffer!
+    var gameBuffer: MTLBuffer!
+    
     
     // Initializer which sets up the Metal device, command queue, and pipeline state
     init(metalKitView: MTKView) {
@@ -21,39 +46,87 @@ class Renderer {
         metalKitView.device = self.device // Assign the device to the MetalKit view
         self.commandQueue = device?.makeCommandQueue() // Create a command queue
         setupPipeline() // Set up the rendering pipeline
+        vertexBuffer = device?.makeBuffer(bytes: Renderer.fullscreenQuad, length: MemoryLayout<SIMD3<Float>>.stride * Renderer.fullscreenQuad.count, options: [])
+        
+        gameBuffer = device?.makeBuffer(bytes: gameBoard, length: MemoryLayout<cell>.stride * gameBoard.count, options: [ ])
     }
     
     // Method to set up the rendering pipeline
     func setupPipeline() {
         let library = device?.makeDefaultLibrary() // Get the default library containing shader code
+        
+        
+        //render pipeline for draw
         let vertexFunction = library?.makeFunction(name: "vertex_main") // Get the vertex shader function
         let fragmentFunction = library?.makeFunction(name: "fragment_main") // Get the fragment shader function
-        
+
         let pipelineDescriptor = MTLRenderPipelineDescriptor() // Create a pipeline descriptor
         pipelineDescriptor.vertexFunction = vertexFunction // Assign the vertex shader function
         pipelineDescriptor.fragmentFunction = fragmentFunction // Assign the fragment shader function
         pipelineDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm // Set the pixel format for color attachments
         
+        //kernel update for world physics
+        let kernelFunction = library?.makeFunction(name: "update_world")
+        let computeDescriptor = MTLComputePipelineDescriptor()
+        computeDescriptor.computeFunction = kernelFunction
+
         do {
-            // Try to create the pipeline state
             pipelineState = try device?.makeRenderPipelineState(descriptor: pipelineDescriptor)
+            computePipelineState = try device?.makeComputePipelineState(descriptor: computeDescriptor, options: [.argumentInfo, .bufferTypeInfo], reflection: nil)
+            
         } catch {
             // Print the error if pipeline state creation fails
             print(error)
+            print("your mom")
         }
+    }
+    
+    //make keyinput buffer
+    //update on key clicks
+    
+    func updateSmoothPLayerLocation(){
+        //update smooth player
+    }
+    
+    func updatePhysics(){
+        //get keyboard buffer state
+        //process inputs
+        
+        //do gameboard and kernel call
+        //gets called on a timer
+        //lock when draw
     }
     
     // Method to perform drawing operations
     func draw(in view: MTKView) {
+        //TODO: do lock stuff
         guard let drawable = view.currentDrawable, // Get the current drawable
               let renderPassDescriptor = view.currentRenderPassDescriptor, // Get the render pass descriptor
               let commandBuffer = commandQueue?.makeCommandBuffer(), // Create a command buffer
-              let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor) else { // Create a render command encoder
+              let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
+        else { // Create a render command encoder
             return
         }
+            
         
         renderEncoder.setRenderPipelineState(pipelineState!) // Set the pipeline state
-        renderEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: 3) // Draw a triangle
+        
+        renderEncoder.setVertexBuffer(
+            vertexBuffer,
+            offset: 0,
+            index: 0)
+        renderEncoder.setFragmentBuffer(
+            device?.makeBuffer(
+                bytes: [
+                    Float(NSApplication.shared.windows.first?.frame.width ?? 0),
+                    Float(NSApplication.shared.windows.first?.frame.height ?? 0)],
+                length: MemoryLayout<Float>.stride * 2,
+                options: [ ]),
+            offset: 0,
+            index: 0)
+        renderEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: Renderer.fullscreenQuad.count)
+
+        
         renderEncoder.endEncoding() // End encoding
         
         commandBuffer.present(drawable) // Present the drawable
